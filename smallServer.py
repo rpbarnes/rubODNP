0d2343c7f0859dbdfb8fa0d1cef5d4376fc3e9fa
root <root@CZC51102Y9.(none)>
2015-07-24 15:14:30 +0200

    smallClient and smallServer are now working versions of the client server pair between the topspin(Windows) and xEpr(centos) computers.

--- smallServer.py
+++ smallServer.py
@@ -1,7 +1,7 @@
 import socket
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-sock.bind(('134.147.66.1',7000))
+sock.bind(('134.147.66.2',7000))
 sock.listen(5)
 while True:
     conn,addr = sock.accept()
