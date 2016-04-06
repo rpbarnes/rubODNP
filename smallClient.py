3fa7d5cf6fb71296ba7903ca7c189d892b8a2c0c
Ryan Barnes <rbarnes@chem.ucsb.edu>
2016-04-06 13:20:01 -0700

    Changed the small client and small server examples to be a littl more extensive.

--- smallClient.py
+++ smallClient.py
@@ -1,9 +1,10 @@
 import socket
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-serverAddress = ('134.147.66.2',7000) # address to the xepr computer
+serverAddress = ('127.0.0.1',9000) # address to the xepr computer
 #serverAddress = ('localhost',7000) # address to the xepr computer
 sock.connect(serverAddress)
-print sock.recv(1024)
+sock.send('This is a command\n')
+print sock.recv(128)
 sock.close()
 
--- smallServer.py
+++ smallServer.py
@@ -1,10 +1,35 @@
 import socket
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-sock.bind(('134.147.66.2',7000))
+sock.bind(('127.0.0.1',9000)) # this will work if coming from computer but I think this will need to be changed to the outside address for functionality with the specman comp.
+### Outside facing ip is 128.111.114.94
 sock.listen(5)
+print "Server is live"
 while True:
     conn,addr = sock.accept()
     print 'Got connection from ', addr
+    incoming = ''
+    command_buffer = []
+    try: # if this faults out lets remake the connection with the host machine
+        temp = conn.recv(128) 
+        print temp
+    except:
+        conn.close()
+        conn, addr = sock.accept()
+        temp = conn.recv(128) # if nothing received the script hangs
+
+    if not temp: break
+    incoming = incoming + temp
+    if incoming[-1] != '\n':
+        if incoming.find('\n'):
+            temp = incoming.split('\n')
+            command_buffer.extend(temp[:-1])
+            incoming = temp[-1]
+        else:
+            break
+    else:
+        command_buffer.extend(incoming.split('\n'))
+        incoming = ''
+    print command_buffer
     conn.send('Thank you for connecting')
     conn.close()
